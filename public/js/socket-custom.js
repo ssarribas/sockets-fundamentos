d6174897fe922b985cc8f3254e321caa2cf670e3
ssarribas <ssarribas@gmail.com>
2020-09-19 18:29:43 +0200

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,22 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Perdimos conexión con el servidor');
+});
+
+// Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Fernando',
+    mensaje: 'Hola mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+});
+
+// Escuchar información
+socket.on('enviarMensaje', function(mensaje) {
+    console.log('Servidor: ', mensaje);
+});
